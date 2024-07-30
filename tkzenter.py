a9a0cccf732ca5bcda01c12eabf6c4a3766d6ec4
aTomWeaver <atomweavermail@gmail.com>
2024-07-30 17:23:17 -0400

    add tracked widget dicts

--- tkzenter.py
+++ tkzenter.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import *
 from tkinter import ttk
 from tkinter import StringVar
 from tkinter import messagebox
@@ -13,3 +13,12 @@ class Tkzenter(Tk):
         super().__init__()
         self.app_title = app_title
         self.title(app_title)
+        self.menubar = Menu(self)
+
+        self.menu_cascades = {}
+        self.rows = {}
+        self.frames = {}
+        self.tkvars = {}
+        self.buttons = {}
+        self.entries = {}
+        self.labels = {}

0ac6ebf3be8027eb0d911b7a239a602b909800a0
aTomWeaver <atomweavermail@gmail.com>
2024-07-31 06:36:52 -0400

    add frame creationg and tkinter variable creation

--- tkzenter.py
+++ tkzenter.py
@@ -22,3 +22,45 @@ class Tkzenter(Tk):
         self.buttons = {}
         self.entries = {}
         self.labels = {}
+
+    def cascades_create(self, list_of_names: list[str]):
+        '''Accepts a list of cascade names and adds them to the menubar.'''
+        for name in list_of_names:
+            cascade = Menu(self.menubar, tearoff=0)
+            self.menubar.add_cascade(label=name, menu=cascade)
+            self.menu_cascades[name] = cascade
+
+    def menubar_add(self, cascade_name: str, list_of_labels_and_fns: list[list]):
+        '''Creates a cascade of cascade_name and adds given labels and
+        functions.
+
+        Each element in list_of_labels_and_fns is a list consisting of a
+        label for the menu option and the function it should run.
+
+        Ex) self.menubar_add("Edit", [["Preferences", edit_prefs],
+                                      ["Presets", edit_presets]])
+        '''
+        if cascade_name not in self.menu_cascades:
+            self.cascades_create([cascade_name])
+        for label, fn in list_of_labels_and_fns:
+            self.menu_cascades[cascade_name].add_command(label=label,
+                                                         command=fn)
+
+    def frame_create(self, root, name: str):
+        self.frames[name] = ttk.Frame(root)
+
+    def tkvar_create(self, kind: str, name: str, initial: str = ""):
+        kinds = {
+                "string": StringVar
+                }
+        self.tkvars[name] = kinds[kind]()
+        if initial:
+            self.tkvars[name].set(initial)
+
+
+if __name__ == "__main__":
+    gui = Tkzenter("Test Gui")
+    gui.menubar_add("Edit", [
+        ["Preferences", lambda: print("editing...")]
+        ])
+    gui.mainloop()
